c342446bb753be8acecd9673e34146d180613f1b
Eleanor-24 <66060352+Eleanor-24@users.noreply.github.com>
2020-12-06 15:40:05 +0800

    Add files via upload

---
+++ imooc-news/common/http.js
@@ -0,0 +1,28 @@
+export default function $http(options) {
+	const {
+		url,
+		data
+	} = options
+	const dataObj = {
+		user_id: '5f941aa5e9422c3e63e70896',
+		...data
+	}
+	return new Promise((reslove, reject) => {
+		uniCloud.callFunction({
+			name: url,
+			data: dataObj
+		}).then((res) => {
+
+			if (res.result.code === 200) {
+				// .then
+				reslove(res.result)
+			} else {
+				// catch
+				reject(res.result)
+			}
+
+		}).catch((err) => {
+			reject(err)
+		})
+	})
+}
